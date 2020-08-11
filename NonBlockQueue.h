16e2a62bd68733e3d46104de744e843f5715510a
MikhailMalkov <65315033+MikhailMalkov@users.noreply.github.com>
2020-08-11 11:07:21 +0300

    Add files via upload

---
+++ NonBlockQueue.h
@@ -0,0 +1,205 @@
+﻿// NonBlockQueue.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#ifndef NONBLOCKQUEUE_H
+#define NONBLOCKQUEUE_H
+
+#include <atomic>
+#include <chrono>
+
+static struct Statistic
+{
+    __int64 enqTime;
+    __int64 deqTime;
+    __int64 interThreadMoveTime;
+};
+
+static const int statArraySize = 1000000;
+
+static Statistic statArray[statArraySize];
+
+#ifdef WIN32
+#include <windows.h>
+inline int GetProcessorSpeed() // MGHr
+{
+    const int OFFSET = 5;
+    const double MHZ = 1000000.0;
+    
+    LARGE_INTEGER qwWait, qwStart, qwCurrent;
+    
+    QueryPerformanceCounter(&qwStart);
+    QueryPerformanceFrequency(&qwWait);
+    qwWait.QuadPart >>= OFFSET;
+    unsigned __int64 Start = __rdtsc();
+    do
+    {
+        QueryPerformanceCounter(&qwCurrent);
+    } while (qwCurrent.QuadPart - qwStart.QuadPart < qwWait.QuadPart);
+    
+    return static_cast<int>(((__rdtsc() - Start) << OFFSET) / MHZ);
+}
+
+#elif __linux__
+#include <x86intrin.h>
+//Starting from GCC 4.5 and later, the __rdtsc() intrinsic is now supported by both MSVCand GCC.
+
+unsigned long long int rdtsc(void) my rtds for any case
+{
+    unsigned long long int x;
+    __asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
+    return x;
+}
+
+inline int GetProcessorSpeed() // MGHr
+{
+    const int TIMEOUT = 250000;
+    const double MHZ = 1000000.0;
+    
+    struct timezone tz;
+    struct timeval tvstart, tvstop;
+    unsigned long long int cycles[2];
+    unsigned long microseconds;
+    
+
+    memset(&tz, 0, sizeof(tz));
+
+    gettimeofday(&tvstart, &tz);
+    cycles[0] = __rdtsc();
+    gettimeofday(&tvstart, &tz);
+
+    usleep(TIMEOUT);
+
+    gettimeofday(&tvstop, &tz);
+    cycles[1] = __rdtsc();
+    gettimeofday(&tvstop, &tz);
+
+    microseconds = ((tvstop.tv_sec - tvstart.tv_sec) * MHZ) + (tvstop.tv_usec - tvstart.tv_usec);
+
+    return static_cast<int>((cycles[1] - cycles[0]) / microseconds);
+}
+#endif
+
+
+
+template<typename T>
+class TNonBlockQueue
+{
+public:
+    TNonBlockQueue(size_t size)
+        : m_buffer( new Item[size]),
+          m_bufferSize(size), 
+          m_enqPos{ 0 },
+          m_deqPos{ 0 }
+    {
+        for (size_t i = 0; i < m_bufferSize; ++i)
+            m_buffer[i].isEmpty.store(1, std::memory_order_relaxed);
+        
+        m_isEnqBusy.store(0, std::memory_order_relaxed);
+        m_isDeqBusy.store(0, std::memory_order_relaxed);
+    }
+
+    ~TNonBlockQueue()
+    {
+        delete[] m_buffer;
+    }
+
+    bool Enqueue(T const& data)
+    {
+        if (uint64_t isBusy = m_isEnqBusy.load(std::memory_order_acquire);  isBusy == 0) // "mutex" to prevent the code from handling more than one producer 
+        {
+            if (!m_isEnqBusy.compare_exchange_strong(isBusy, 1, std::memory_order_relaxed))
+                return false; // another producer was faster
+            _int64 startTime = __rdtsc();
+            
+            if (m_enqPos >= m_bufferSize) // moving enqueue cursor to the start position
+                m_enqPos = 0;
+
+            Item* pItem = &m_buffer[m_enqPos];
+            bool result{ false };
+            
+            if (pItem)
+            {
+                if (pItem->isEmpty.load(std::memory_order_acquire) == 1) // "mutex" for isEmpty item marker to synchronize producer and consumer thread
+                {
+                    pItem->data = data; // put data
+                    ++m_enqPos;
+                    pItem->isEmpty.store(0, std::memory_order_release);
+                    pItem->enqTime = __rdtsc();
+                    if (m_enqPos <= statArraySize)
+                        statArray[m_enqPos - 1].enqTime = pItem->enqTime - startTime;
+                    result = true;
+                }
+            }
+            
+            m_isEnqBusy.store(0, std::memory_order_release);
+            return result;
+        }
+        
+        return false;
+    }
+
+    bool Dequeue(T& data)
+    {
+        if (uint64_t isBusy = m_isDeqBusy.load(std::memory_order_acquire); isBusy == 0) // "mutex" to prevent the code from handling more than one consumer 
+        {
+            if (!m_isDeqBusy.compare_exchange_strong(isBusy, 1, std::memory_order_relaxed))
+                return false; // another consumer was faster
+            
+            _int64 startTime = __rdtsc();
+            
+            if (m_deqPos >= m_bufferSize) // moving dequeue cursor to the start position
+                m_deqPos = 0;
+
+            Item* pItem = &m_buffer[m_deqPos];
+            bool result{ false };
+
+            if (pItem)
+            {
+                if (pItem->isEmpty.load(std::memory_order_acquire) == 0) // "mutex" for isEmpty item marker to synchronize producer and consumer thread
+                {
+                    data = pItem->data; // get data
+                    ++m_deqPos;
+                    pItem->isEmpty.store(1, std::memory_order_release);
+                    if (m_deqPos <= statArraySize)
+                    {
+                        statArray[m_deqPos - 1].deqTime = __rdtsc() - startTime;
+                        statArray[m_deqPos - 1] .interThreadMoveTime = __rdtsc() - pItem->enqTime;
+                    }
+                    result = true;
+                }
+
+                
+            }
+
+            m_isDeqBusy.store(0, std::memory_order_release);
+            return result;
+        }
+        
+        return false;
+    }
+    
+    TNonBlockQueue(TNonBlockQueue const&) = delete;
+    void operator = (TNonBlockQueue const&) = delete;
+   
+private:
+    
+    struct alignas(64) Item
+    {
+        std::atomic<uint64_t> isEmpty;
+        __int64 enqTime;
+        T data;
+    };
+    
+    Item* const m_buffer;
+    uint64_t const m_bufferSize;
+
+    uint64_t m_enqPos;
+    uint64_t m_deqPos;
+    
+    std::atomic<uint64_t> m_isEnqBusy; 
+    std::atomic<uint64_t> m_isDeqBusy;
+    
+};
+
+#endif
+
---
+++ main.cpp
@@ -0,0 +1,103 @@
+#include "NonBlockQueue.h"
+#include <thread>
+#include <iostream>
+#include <memory>
+int main()
+{
+    size_t sizeQueue = 10;
+
+    TNonBlockQueue<int> queue(sizeQueue);
+
+   std::shared_ptr<std::thread>(
+       new std::thread ([&]()
+       { 
+           for (size_t i = 0; i < sizeQueue; ++i)
+              queue.Enqueue(i);
+       }), 
+       [](std::thread* pthread) 
+       {
+           if (pthread->joinable()) pthread->join();
+       }
+   );
+    
+   std::shared_ptr<std::thread>(
+       new std::thread([&]()
+           {
+               int item{-1};
+               for (size_t i = 0; i < sizeQueue; ++i)
+               {
+                   queue.Dequeue(item);
+               }
+           }),
+       [&](std::thread* pthread)
+       {
+            if (pthread->joinable()) pthread->join();
+            
+            std::cout << std::endl << "Statistics: " << std::endl << std::endl;
+            
+            __int64 sumEnqTime{ 0 };
+            __int64 sumDeqTime{ 0 };
+            __int64 sumInterThreadTime{0};
+            
+            
+            __int64 MaxEnqTime{ 0 };
+            __int64 MaxDeqTime{ 0 };
+            __int64 MaxInterThreadTime{ 0 };
+
+            __int64 MinEnqTime{ 0 };
+            __int64 MinDeqTime{ 0 };
+            __int64 MinInterThreadTime{ 0 };
+            
+            const int processorSpeedMHZ = GetProcessorSpeed();
+            
+            for (size_t i = 0; i < statArraySize && i < sizeQueue; ++i)
+            {
+                if (MaxEnqTime < statArray[i].enqTime || !i)
+                    MaxEnqTime = statArray[i].enqTime;
+                
+                if(MaxDeqTime < statArray[i].deqTime || !i)
+                    MaxDeqTime = statArray[i].deqTime;
+                
+                if (MaxInterThreadTime < statArray[i].interThreadMoveTime || !i)
+                    MaxInterThreadTime = statArray[i].interThreadMoveTime;
+
+                if (MinEnqTime > statArray[i].enqTime || !i)
+                    MinEnqTime = statArray[i].enqTime;
+
+                if (MinDeqTime > statArray[i].deqTime || !i)
+                    MinDeqTime = statArray[i].deqTime;
+
+                if (MinInterThreadTime > statArray[i].interThreadMoveTime || !i)
+                    MinInterThreadTime = statArray[i].interThreadMoveTime;
+                
+                sumEnqTime += statArray[i].enqTime;
+                sumDeqTime += statArray[i].deqTime;
+                sumInterThreadTime += statArray[i].interThreadMoveTime;
+                
+                std::cout << std::endl << "Index of item = " << i << std::endl;
+                
+                std::cout << std::endl << "Enque time = " << statArray[i].enqTime/processorSpeedMHZ << " nanoseconds " << std::endl;
+                std::cout << "Deque time = " << statArray[i].deqTime/ processorSpeedMHZ << " nanoseconds " << std::endl;
+                std::cout << "Inter-thread moving time = " << statArray[i].interThreadMoveTime/ processorSpeedMHZ << " nanoseconds " << std::endl;
+                
+               
+            }
+            
+            std::cout << std::endl << "Max enque time = " << MaxEnqTime / processorSpeedMHZ << " nanoseconds " << std::endl;
+            std::cout << "Max deque time = " << MaxDeqTime / processorSpeedMHZ << " nanoseconds " << std::endl;
+            std::cout << "Max Inter-thread moving time = " << MaxInterThreadTime / processorSpeedMHZ << " nanoseconds " << std::endl;
+            
+            std::cout << std::endl << "Min enque time = " << MinEnqTime / processorSpeedMHZ << " nanoseconds " << std::endl;
+            std::cout << "Min deque time = " << MinDeqTime / processorSpeedMHZ << " nanoseconds " << std::endl;
+            std::cout << "Min Inter-thread moving time = " << MinInterThreadTime / processorSpeedMHZ << " nanoseconds " << std::endl;
+            
+            std::cout << std::endl << "Average enque time = " << (sumEnqTime / sizeQueue)/ processorSpeedMHZ << " nanoseconds " << std::endl;
+            std::cout << "Average deque time = " << (sumDeqTime / sizeQueue)/ processorSpeedMHZ << " nanoseconds " << std::endl;
+            std::cout << "Average Inter-thread moving time = " << (sumInterThreadTime / sizeQueue)/ processorSpeedMHZ << " nanoseconds " << std::endl;
+            
+       }
+   );
+    
+   
+    return 0;
+}
